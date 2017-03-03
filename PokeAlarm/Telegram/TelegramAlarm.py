5f009039b916d43d2373cd2535ab487a275b715a
LORD-SS <slgphantom@gmail.com>
2017-03-04 05:28:58 +0800

    again.

--- PokeAlarm/Telegram/TelegramAlarm.py
+++ PokeAlarm/Telegram/TelegramAlarm.py
@@ -101,7 +101,7 @@ class TelegramAlarm(Alarm):
             what_type = imghdr.what(image_file_obj)
             if what_type is not None:
                 videoargs = {
-                    'chat_id': alert['channel'],
+                    'chat_id': alert['chat_id'],
                     'video': video_url,
                     'width': 192,
                     'height': 192,
@@ -118,7 +118,7 @@ class TelegramAlarm(Alarm):
                 what_type = imghdr.what(image_file_obj)
                 if what_type is not None:
                     videoargs = {
-                        'chat_id': alert['channel'],
+                        'chat_id': alert['chat_id'],
                         'video': video_url,
                         'width': 192,
                         'height': 192,
@@ -129,7 +129,7 @@ class TelegramAlarm(Alarm):
                 else: # fallback to sticker
                     if sticker_id:
                         stickerargs = {
-                            'chat_id': alert['channel'],
+                            'chat_id': alert['chat_id'],
                             'sticker': unicode(sticker_id),
                             'disable_notification': 'True'
                         }
